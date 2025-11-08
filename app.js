142738c20c874a68c67ed374feb5c158a3a48f89
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2025-11-08 22:28:47 +0600

    method

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 console.log("hello vue!!");
 const app = Vue.createApp({
-    template: `<h1>Hi I am from vue template</h1>`
-})
-app.mount("#app")
+  // template: `<h1>Hi I am from vue template</h1>`,
+  // dynamic data using data function
+  data() {
+    return {
+      title: "Hello from Vue Data",
+      author: "Priyanka Das ",
+      age: 15,
+      designation: "MERN Stack Developer",
+    };
+  },
+  methods: {
+    changeTitle() {
+      console.log("You just click me ");
+      this.title = "Title changed from Vue Method";
+    },
+  },
+});
+app.mount("#app");
