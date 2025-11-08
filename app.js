6c0dd5dadb3645c5452e31323570a05fc9a9c050
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2025-11-09 00:22:11 +0600

    mouse event handler

--- app.js
+++ app.js
@@ -4,17 +4,31 @@ const app = Vue.createApp({
   // dynamic data using data function
   data() {
     return {
+      showDetails: true,
       title: "Hello from Vue Data",
       author: "Priyanka Das ",
       age: 15,
       designation: "MERN Stack Developer",
+      x: 0,
+      y: 0,
     };
   },
   methods: {
-    changeTitle() {
-      console.log("You just click me ");
-      this.title = "Title changed from Vue Method";
+    changeTitle(title) {
+      //   console.log("You just click me ");
+      //   this.title = "Title changed from Vue Method";
+      this.title = title;
     },
+    toggleShowBooks() {
+      this.showDetails = !this.showDetails;
+    },
+    handleEvent(e) {
+      console.log(e,"You just clicked me!!");
+    },
+    handleMouseMove(e) {
+        this.x = e.offsetX;
+        this.y = e.offsetY;
+    }
   },
 });
 app.mount("#app");
